3efdd3efbf615e1e327b81a8c69d2dd35ef05cb8
NailMinnigalin <DisappearedCat@yandex.ru>
2024-01-16 13:22:11 +0300

    implement

--- HelloWorldSwashbuckle/Program.cs
+++ HelloWorldSwashbuckle/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,13 @@ builder.Services.AddControllers().AddApplicationPart(typeof(ProductsModule.Produ
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 List<string> groups = new();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Title = "Test title"
+	});
+});
 
 var app = builder.Build();
 
